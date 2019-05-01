f98feb6b380ce594385313cf4d3f31a226a7416b
RushilDaya <rushildayabme@gmail.com>
2019-05-01 12:04:29 +0200

    added the block drawing

--- experiment/onOffStimulation.m
+++ experiment/onOffStimulation.m
@@ -8,7 +8,6 @@ TARGETS = getTargets();
 TRIAL_SEQUENCE = getSequence(TARGETS, trialsPerFrequency);
 HEADER_DISPLAY = getHeaderDisplays(TARGETS, frameRate);
 FLICKER_DISPLAY = getFlickerDisplays(TARGETS, frameRate, eventsPerTrial,eventLengthSeconds ,frequencies);
-
 runProcess(TARGETS, TRIAL_SEQUENCE, HEADER_DISPLAY, FLICKER_DISPLAY);
 
 end
@@ -123,7 +122,76 @@ defaultPriority = Priority();
         screenNumber = max(Screen('Screens'));
         [window, screenRect]=Screen('OpenWindow', screenNumber,0);
         
+        
+        
         %% the code for running the stimulator goes here
+        
+        % determine screen block sizes and loop increments etc
+        OUTER_MARGIN_FRACTION = 0.1;
+        topBottomMargin = OUTER_MARGIN_FRACTION*RectHeight(screenRect);
+        leftRightMargin = OUTER_MARGIN_FRACTION*RectWidth(screenRect);
+        [numFrequencies, targetsPerFrequency] = size(targets);
+        
+        % the outer rectangular block with margin and padding ( the actual target must
+        % be square
+        targetBoxHeightMargined = RectHeight(screenRect)*(1-OUTER_MARGIN_FRACTION*2)/numFrequencies;
+        targetBoxWidthMargined = RectWidth(screenRect)*(1-OUTER_MARGIN_FRACTION*2)/targetsPerFrequency;
+        
+        % within each of the target boxes we need some small amount of
+        % padding
+        % following this we need to draw the true stimulator
+        INNER_MARGIN_FRACTION=0.1;
+        targetBoxTopBottomMargin= targetBoxHeightMargined*INNER_MARGIN_FRACTION;
+        targetBoxLeftRightMargin= targetBoxWidthMargined*INNER_MARGIN_FRACTION;
+        targetBoxHeightNoMargin = targetBoxHeightMargined*(1-2*INNER_MARGIN_FRACTION);
+        targetBoxWidthNoMargin = targetBoxWidthMargined*(1-2*INNER_MARGIN_FRACTION);
+        
+        stimulatorHeight = min(targetBoxHeightNoMargin, targetBoxWidthNoMargin);
+        stimulatorWidth = min(targetBoxHeightNoMargin, targetBoxWidthNoMargin);
+        
+        targetCenters = zeros(numFrequencies, targetsPerFrequency,2);
+        for i = 1:numFrequencies
+            for j = 1:targetsPerFrequency
+                %first is the vertical position
+                targetCenters(i,j,1)= topBottomMargin+(i-1)*targetBoxHeightMargined+targetBoxTopBottomMargin + targetBoxHeightNoMargin/2; % still need to incorporate the side padding
+                %second is the horizontal position
+                targetCenters(i,j,2)= leftRightMargin+(j-1)*targetBoxWidthMargined+targetBoxLeftRightMargin + targetBoxWidthNoMargin/2;
+            end
+        end
+        
+        
+        baseStimulator = [0, 0, stimulatorHeight, stimulatorWidth];
+        allStimulators = nan(4,numFrequencies*targetsPerFrequency);
+        for i = 1:numFrequencies
+            for j = 1:targetsPerFrequency
+                allStimulators(:, (i-1)*targetsPerFrequency+j) = CenterRectOnPointd(baseStimulator,targetCenters(i,j,2),targetCenters(i,j,1));
+            end
+        end
+        
+        allColors = [255 255 255;
+                     255 1 1;
+                     1 255 1;
+                     230 1 1;
+                     1 1 255;
+                     1 1 255;
+                     1 255 1;
+                     1 255 1;
+                     1 255 255
+        ]';
+        Screen('FillRect',window, allColors, allStimulators);
+        Screen('Flip',window);
+        KbStrokeWait;
+        
+      
+        
+        % actual stimulation
+%         for trialIdx = 1:length(trialSequence)
+%             % display Header
+%             structKey = int2str(trialSequence(trialIdx));
+%             
+%         end
+     
+        
         
     catch
         Screen('CloseAll');
